71750f8229fea09368971ce106f4d8a808a48eca
LucasAraujo639 <lucas.ezequiel.321@gmail.com>
2023-10-01 12:04:01 -0300

    Se agregaron un par de tests

--- lista_test.go
+++ lista_test.go
@@ -1,7 +1,7 @@
 package lista_test
 
 import (
-	TDALista "tdas/lista"
+	TDALista "lista"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,18 +11,18 @@ func TestListaVacia(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
 
 	require.True(t, lista.EstaVacia())
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerPrimero()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerUltimo()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.BorrarPrimero()})
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerPrimero() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerUltimo() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.BorrarPrimero() })
 	require.Equal(t, 0, lista.Largo())
-	
+
 	iter := lista.Iterador()
 
 	require.False(t, iter.HaySiguiente())
 	require.True(t, lista.EstaVacia())
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerPrimero()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerUltimo()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.BorrarPrimero()})
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerPrimero() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerUltimo() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.BorrarPrimero() })
 	require.Equal(t, 0, lista.Largo())
 }
 
@@ -39,7 +39,7 @@ func TestUnicoValor(t *testing.T) {
 
 	doble := 0
 	lista.Iterar(func(x int) bool {
-		doble = 2*x
+		doble = 2 * x
 		return true
 	})
 
@@ -48,7 +48,7 @@ func TestUnicoValor(t *testing.T) {
 	require.Equal(t, 1, lista.Largo())
 	require.Equal(t, 20, lista.VerPrimero())
 	require.Equal(t, 20, lista.VerUltimo())
-	
+
 	iter := lista.Iterador()
 
 	require.True(t, iter.HaySiguiente())
@@ -58,12 +58,12 @@ func TestUnicoValor(t *testing.T) {
 
 	require.False(t, iter.HaySiguiente())
 	require.Equal(t, 20, lista.BorrarPrimero())
-	require.True(t, lista.EstaVacia())
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerPrimero()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerUltimo()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.BorrarPrimero()})
+	require.True(t, lista.EstaVacia()) // aca falla pero nose porque
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerPrimero() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerUltimo() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.BorrarPrimero() })
 	require.Equal(t, 0, lista.Largo())
-	
+
 	iter = lista.Iterador()
 
 	require.False(t, iter.HaySiguiente())
@@ -78,7 +78,7 @@ func TestUnicoValor(t *testing.T) {
 
 	doble = 0
 	lista.Iterar(func(x int) bool {
-		doble = 2*x
+		doble = 2 * x
 		return true
 	})
 
@@ -87,7 +87,7 @@ func TestUnicoValor(t *testing.T) {
 	require.Equal(t, 1, lista.Largo())
 	require.Equal(t, 33, lista.VerPrimero())
 	require.Equal(t, 33, lista.VerUltimo())
-	
+
 	iter = lista.Iterador()
 
 	require.True(t, iter.HaySiguiente())
@@ -98,12 +98,50 @@ func TestUnicoValor(t *testing.T) {
 	require.False(t, iter.HaySiguiente())
 	require.Equal(t, 33, lista.BorrarPrimero())
 	require.True(t, lista.EstaVacia())
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerPrimero()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.VerUltimo()})
-	require.PanicsWithValue(t, "La lista esta vacia", func() {lista.BorrarPrimero()})
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerPrimero() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerUltimo() })
+	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.BorrarPrimero() })
 	require.Equal(t, 0, lista.Largo())
-	
+
 	iter = lista.Iterador()
 
 	require.False(t, iter.HaySiguiente())
 }
+
+func VerPrimeroYUltimo(t *testing.T) {
+	t.Log("Hacemos pruebas acolando y desacolando y ver si salen en el orden deseado")
+	lista := TDALista.CrearListaEnlazada[int]()
+	lista.InsertarPrimero(1)
+	require.EqualValues(t, 1, lista.VerPrimero())
+	require.EqualValues(t, 1, lista.VerUltimo())
+	lista.InsertarPrimero(2)
+	lista.InsertarUltimo(3)
+	require.EqualValues(t, 2, lista.VerPrimero())
+	require.EqualValues(t, 3, lista.VerUltimo())
+}
+
+func InsertarPrimeroYBorrar(t *testing.T) {
+	t.Log("Hacemos pruebas acolando y desacolando y ver si salen en el orden deseado")
+	lista := TDALista.CrearListaEnlazada[int]()
+	lista.InsertarPrimero(1)
+	lista.InsertarPrimero(2)
+	lista.InsertarPrimero(3)
+	lista.BorrarPrimero()
+	require.EqualValues(t, 2, lista.VerPrimero())
+	require.EqualValues(t, 1, lista.VerUltimo())
+}
+func InsertarUltimoYBorrar(t *testing.T) {
+	t.Log("Hacemos pruebas acolando y desacolando y ver si salen en el orden deseado")
+	lista := TDALista.CrearListaEnlazada[int]()
+	lista.InsertarUltimo(1)
+	lista.InsertarUltimo(2)
+	lista.InsertarUltimo(3)
+	lista.BorrarPrimero()
+	require.EqualValues(t, 2, lista.VerPrimero())
+	require.EqualValues(t, 3, lista.VerUltimo())
+
+}
+
+// func TestVolumen(t *testing.T) {
+
+// }
